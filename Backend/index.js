592f37dced56433f52d94bfcdcd57f0d1c8410f0
Mikko <lancerthortan@gmail.com>
2023-07-22 19:45:37 +0800

    Update build

--- Backend/index.js
+++ Backend/index.js
@@ -24,18 +24,18 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/post', router);
 app.use('/api/users', userRouter);
 
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//     const __dirname = path.resolve();
+//     app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    );
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running....');
-    });
-}
+//     app.get('*', (req, res) =>
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+//     );
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('API is running....');
+//     });
+// }
 
 //error handler
 app.use(errorHandler)
